0359953749f8f77208cda558dd557509384d96ff
Ayush Shekhar <ayush221b@gmail.com>
2019-10-05 09:15:35 +0530

    Tested twitter setup, proceed to github setup

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var Twitter = require('twitter');
 var config = require('./config.js');
 
-var T = new Twitter(config);
+var client = new Twitter(config);
 
+client
+	.post('statuses/update', { status: 'Hello World, I Love Twitter!' })
+	.then(function(tweet) {
+		console.log(tweet);
+	})
+	.catch(function(error) {
+		throw error;
+	});
