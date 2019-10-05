2012caff1e41defa1b2dff35058bc382d8dfd047
Ayush Shekhar <ayush221b@gmail.com>
2019-10-05 09:01:06 +0530

    Initial project setup

---
+++ config-sample.js
@@ -0,0 +1,8 @@
+// Fill in the all the values, then rename this file to config.js
+
+module.exports = {
+	consumer_key: '',
+	consumer_secret: '',
+	access_token_key: '',
+	access_token_secret: ''
+};
