f9f199f35cd63a846b5ee1f5a4a42d0642d0f8db
Ayush Shekhar <ayush221b@gmail.com>
2019-10-06 09:04:32 +0530

    Initial setup done, move on to logic

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 var Twitter = require('twitter');
 var config = require('./config.js');
 
+var cron = require('node-cron');
+var axios = require('axios');
+var express = require('express');
+
+const app = express();
+
 var client = new Twitter(config);
 
-client
-	.post('statuses/update', { status: 'Hello World, I Love Twitter!' })
-	.then(function(tweet) {
-		console.log(tweet);
-	})
-	.catch(function(error) {
-		throw error;
-	});
+
+
+app.get('/', (req, res) => {
+	res.json('it is working!');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`Up and running on port ${process.env.PORT}`);
+});
---
+++ config.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+
+module.exports = {
+	consumer_key: process.env.CONSUMER_KEY,
+	consumer_secret: process.env.CONSUMER_SECRET,
+	access_token_key: process.env.ACCESS_TOKEN_KEY,
+	access_token_secret: process.env.ACCESS_TOKEN_SECRET
+};
+
+// The actual values are specified in the .env file
